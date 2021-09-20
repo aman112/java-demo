011ae1a397a0ee7b7a7be10c00c2e1be1e94221a
Aman Aggarwal <aman.aggarwal195gmail.com>
2021-09-20 22:59:39 +0530

    Implementation one added from Java Demo master

---
+++ src/com/demo/implementations/ImplementationOne.java
@@ -0,0 +1,12 @@
+package com.demo.implementations;
+
+import com.demo.interfaces.DemoInterface;
+
+public class ImplementationOne implements DemoInterface {
+
+	@Override
+	public void printValues() {
+		System.out.println("Implemented from Java Demo project on branch master");
+		
+	}
+}

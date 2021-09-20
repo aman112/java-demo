324e75d1b3f4cc807b91985af4c47cd8f865607c
Aman Aggarwal <aman.aggarwal195gmail.com>
2021-09-20 23:00:08 +0530

    Implemetation One added from Java demo 1 interface

---
+++ src/com/demo/implementations/ImplementationOne.java
@@ -0,0 +1,13 @@
+package com.demo.implementations;
+
+import com.demo.interfaces.DemoInterface;
+
+public class ImplementationOne implements DemoInterface {
+
+	@Override
+	public void printValues() {
+		System.out.println("Implemented from Java Demo 1 project on branch interface");
+		
+	}
+
+}
